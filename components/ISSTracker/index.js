059ec8a386185abcace7b6a4fb3eb4bc9d56ad0f
Niko Kreissl <nikokreissl@gmail.com>
2023-03-07 11:36:51 +0100

    Finish exercise

--- components/ISSTracker/index.js
+++ components/ISSTracker/index.js
@@ -1,36 +1,15 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 import Controls from "../Controls/index";
 import Map from "../Map/index";
+import useSWR from "swr";
 
 const URL = "https://api.wheretheiss.at/v1/satellites/25544";
 
 export default function ISSTracker() {
-  const [coords, setCoords] = useState({
-    longitude: 0,
-    latitude: 0,
-  });
+  const { data: coords, error, isLoading, mutate } = useSWR(URL);
 
-  async function getISSCoords() {
-    try {
-      const response = await fetch(URL);
-      if (response.ok) {
-        const data = await response.json();
-        setCoords({ longitude: data.longitude, latitude: data.latitude });
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
-  useEffect(() => {
-    const timer = setInterval(() => {
-      getISSCoords();
-    }, 5000);
-
-    return () => {
-      clearInterval(timer);
-    };
-  }, []);
+  if (error) return <div>Failed to load</div>;
+  if (isLoading) return <div>Loading...</div>;
 
   return (
     <main>
@@ -38,7 +17,7 @@ export default function ISSTracker() {
       <Controls
         longitude={coords.longitude}
         latitude={coords.latitude}
-        onRefresh={getISSCoords}
+        onRefresh={() => mutate()}
       />
     </main>
   );
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,25 @@
 import GlobalStyle from "../styles";
+import { SWRConfig } from "swr";
+
+const fetcher = async (url) => {
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    const error = new Error("Failed to load");
+    error.info = await response.json();
+    error.status = response.status;
+    throw error;
+  }
+  return response.json();
+};
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <GlobalStyle />
-      <Component {...pageProps} />
+      <SWRConfig value={{ fetcher, refreshInterval: 5000 }}>
+        <Component {...pageProps} />
+      </SWRConfig>
     </>
   );
 }
